c07b5d4d678293e720d7d27a38250304477d4fbf
ahmed <ahmedfrancilien@gmail.com>
2024-02-10 16:52:13 +0100

    Abstract class for configuration with concrete methods

--- src/test/java/com/asxms/TestContainersTest.java
+++ src/test/java/com/asxms/TestContainersTest.java
@@ -1,37 +1,16 @@
 package com.asxms;
 
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestContainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("asxms-dao-unit-test")
-                    .withUsername("asxms")
-                    .withPassword("password");
-
+public class TestContainersTest extends AbstractTestcontainer {
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isCreated()).isTrue();
         assertThat(postgreSQLContainer.isRunning()).isTrue();
-    }
-
-    @Test
-    void canApplyDBMigrationsWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
     }
 }
