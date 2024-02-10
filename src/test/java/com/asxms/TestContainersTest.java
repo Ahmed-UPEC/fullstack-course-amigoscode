57611ee6852b33234917d44f74050176d62e96d6
ahmed <ahmedfrancilien@gmail.com>
2024-02-10 15:04:05 +0100

    First JUnit test

--- src/test/java/com/asxms/TestContainersTest.java
+++ src/test/java/com/asxms/TestContainersTest.java
@@ -1,10 +1,26 @@
 package com.asxms;
 
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
 public class TestContainersTest {
 
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("asxms-dao-unit-test")
+                    .withUsername("asxms")
+                    .withPassword("password");
+
     @Test
-    void name() {
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
     }
 }
