97d9d4a951b8556b95acead9481a01dde237b8d6
Ahmed-UPEC <56971722+Ahmed-UPEC@users.noreply.github.com>
2024-02-19 12:51:11 +0100

    frontend update

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,25 +1,60 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from "react";
 
-import SidebarWithHeader from './shared/Sidebar'
-import { getCustomers } from './services/client'
+import SidebarWithHeader from "./components/shared/Sidebar";
+import { getCustomers } from "./services/client";
+import { Spinner } from "@chakra-ui/react";
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import SocialProfileWithImage from "./components/shared/CustomerCard";
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getCustomers().then((res) => {
-      console.log(res)
-    })
-    .catch((e) => {
-      console.log(e)
-    })
-  }, [])
+    setLoading(true);
+
+    getCustomers()
+      .then((res) => {
+        console.log(res);
+        setCustomers(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .then(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
       <SidebarWithHeader>
+        {customers.length === 0 && loading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : (
+          <>
+            <Wrap spacing='30px' justify='center'>
+            {customers.map((customer) => (
+              <WrapItem key={customer.id}>
+                <SocialProfileWithImage
+                  name={customer.name}
+                  email={customer.email}
+                  age={customer.age}
+                />
+              </WrapItem>
+            ))}
+            </Wrap>
+          </>
+        )}
       </SidebarWithHeader>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
